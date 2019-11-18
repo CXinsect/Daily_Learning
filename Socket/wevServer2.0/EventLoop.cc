ca305d3251554f8223e5cfb04faff9b5f2916c27
cxinsect <1718060998@qq.com>
2019-11-18 23:23:03 +0800

    Daily:Modify Epoll

---
+++ Socket/wevServer2.0/EventLoop.cc
@@ -0,0 +1,45 @@
+#include "EventLoop.h"
+#include "Channel.cc"
+// #include "Poller.cc"
+// int kPollTimeMs = 5*1000;
+void EventLoop::loop() {
+    assert(!looping_);
+    looping_ = true;
+    quit_ = false;
+    while(!quit_) {
+        std::cout << "Loop" << std::endl;
+        activeChannels_.clear();
+        poller_->poll(&activeChannels_);
+        eventHanding_ = true;
+        for(ChannelList::iterator it = activeChannels_.begin();
+            it != activeChannels_.end();it++) {
+                (*it)->handleEvent();
+            }
+        eventHanding_ = false;
+    }
+    std::cout << "Event is going to be Stoped" << std::endl;
+    looping_ = false;
+}
+void EventLoop::updateChannel(Channel * channel) {
+    assert(channel->owerLoop() == this);
+    poller_->updateChannel(channel);
+}
+void EventLoop::quit() {
+    quit_ = true;
+}
+void EventLoop::runInLoop(const CallBack &cb) {
+    cb();
+}
+void EventLoop::removeChannel(Channel * channel_) {
+    assert(channel_->owerLoop() == this);
+    if(eventHanding_) {
+        assert(std::find(activeChannels_.begin(),activeChannels_.end(),channel_) == activeChannels_.end());
+    }
+    poller_->removeChannel(channel_);
+}
+EventLoop::~EventLoop() {
+    std::cout << "~EventLoop begin" << std::endl;
+}
+void EventLoop::queueLoop(lifeCycle cb) {
+    pendingEvent_.push_back(std::move(cb));
+}
